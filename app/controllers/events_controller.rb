0b451c10aa07ca5c8f72741249d695f3d1e3e1f9
Gabriel Leung <gabriel.leung.607@gmail.com>
2015-03-28 16:23:12 -0700

    merge

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,20 +2,29 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new
+    s_year = params["start_date"]["year"]
+    s_month = params["start_date"]["month"]
+    s_day = params["start_date"]["day"]
     s_hour = params["start_time"]["hour"]
     s_minute = params["start_time"]["minute"]
-    s_time = s_hour + ":" + "s_minute"
+    s_time = s_hour + ":" + s_minute
+    s_date =  s_day + s_month + s_year 
+    binding.pry
     e_hour = params["end_time"]["hour"]
     e_minute = params["end_time"]["minute"]
-    e_time = e_hour + ":" + "e_minute"
+    e_time = e_hour + ":" + e_minute
     @event.name = params["Name"]
     @event.location = params["Location"]
     @event.cost = params["Cost"]
     @event.start = s_time
     @event.end = e_time
-
+    binding.pry
     @event.save
 
     render :details
   end
+
+
+
+
 end
