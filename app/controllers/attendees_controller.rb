be15a972d51ea4d90c30d38db71db6dd287af9dc
Emily Craig <emrcraig@gmail.com>
2015-03-29 04:55:25 -0700

    style event form

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -14,14 +14,15 @@ class AttendeesController < ApplicationController
 			attendee = Attendee.create(email: invitee.email, contact_id: invitee.id, event_id: @event.id );
 			attendee_id = attendee.id.to_s
 
-			email_body = "<html><h1>Hey <strong>"+attendee.contact.firstname+"</strong>, how are you?</h1><p> You've been invited by <strong>"+current_user.firstname+"</strong> to the event <strong>"+attendee.event.name+"</strong> on Go Dutch! <a href='http://localhost:3000/payments/new/"+attendee_id+"'>Follow this link to participate!</a><p></html>"
+			email_body = "<html><head><style> div { font-family: 'Open Sans', sans-serif; color: #111; border: solid 2px #81B7FF, padding: 10px} h2 { background-color: #81B7FF, padding: 10px, font-size: 20px; color: #fff; } a { font-size: 14px; color: #60c0ac; text-decoration: none; } </style></head><body>
+				<div><h2>Hey <strong>"+attendee.contact.firstname+"</strong>, how are you?</h2><p> You've been invited by <strong>"+current_user.firstname+"</strong> to the event <strong>"+attendee.event.name+"</strong> on Go Dutch! <a href='http://localhost:3000/payments/new/"+attendee_id+"'>Follow this link to participate!</a><p><br></div></body></html>"
 
 
 	    m = Mandrill::API.new
 			message = {  
 			 :subject=> "Hey "+r_firstname+", wanna go to "+attendee.event.name+"?",  
-			 :from_name=> "Your name",  
-			 :text=>"Hi message, how are you?",  
+			 :from_name=> "Go Dutch App",  
+			 :text=>"Hi, how are you?",  
 			 :to=>[  
 			   {  
 			     :email=> "godutchteam@gmail.com", #should be r_email  
