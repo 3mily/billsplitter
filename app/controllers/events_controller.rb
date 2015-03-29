f5236784dee1c2fa760299b04ad3fa06fe9c2a39
Emily Craig <emrcraig@gmail.com>
2015-03-29 10:18:54 -0700

    remove End Date from Event Form, add table to Payment New

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,13 +11,11 @@ class EventsController < ApplicationController
 
     @event = Event.new
     start_time = @event.format_datetime(params["start_date"],params["start_time"])
-    end_time = @event.format_datetime(params["end_date"],params["end_time"])
     @event.name = params["Name"]
     @event.location = params["Location"]
     @event.cost = params["Cost"]
     @event.details = params["Description"]
     @event.start = start_time
-    @event.end = end_time
     @event.user_id = current_user.id
     @event.save
     response.headers['X-PJAX-URL'] = "http://localhost:3000/events/invite"
